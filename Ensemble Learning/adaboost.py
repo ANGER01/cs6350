755de052ec819ce3423e08e79e04793569cbfcaf
Anger <anger6701@gmail.com>
2024-10-15 23:08:46 -0600

    stuff

---
+++ Ensemble Learning/adaboost.py
@@ -0,0 +1,22 @@
+from DecisionTree.decision_tree import DecisionTree
+import os
+import numpy as np
+import pandas as pd
+from Helpers.helpers import get_data
+
+Bank_X_train, Bank_y_train, Bank_X_test, Bank_y_test = get_data("bank")
+
+def create_stump(x, y) -> DecisionTree:
+    """Takes cleaned data(Not Numerical, only catagorical)
+
+    Args:
+        x (ndarray): pandas data frame with feature values
+        y (ndarray): pandas series with labels
+
+    Returns:
+        DecisionTree: instance of decision tree
+    """
+    stump = DecisionTree(max_depth=1)
+    stump.fit(x, y, func="Entropy")
+    return stump
+
